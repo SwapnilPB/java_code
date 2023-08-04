0cf84af40118354e998412ba2d3efc81c9e30bb7
Swapnil Bhojane <swapnil.bhojane@sarvatra.in>
2023-08-04 07:21:02 +0530

    Added more classes

---
+++ src/javaprac/ReverseStringN.java
@@ -0,0 +1,17 @@
+package javaprac;
+
+public class ReverseStringN {
+    public static void main(String[] args) {
+        String rev= revString("euhf");
+        System.out.println("rev is "+rev);
+
+    }
+    static String revString(String st){
+        String rev ="";
+        for (int i=0;i<st.length();i++){
+            char ch = st.charAt(i);
+            rev=ch+rev;
+        }
+        return rev;
+    }
+}
